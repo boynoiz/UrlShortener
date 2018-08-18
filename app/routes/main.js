c6577375f242f345fbd630c575ba29a23a4346b9
Pathompong Pechkongtong <boynoiz@gmail.com>
2018-08-18 11:32:02 +0700

    Change module's name, apply to es6

--- app/routes/main.js
+++ app/routes/main.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const router = express.Router();
-const mainController = require('../controllers/main');
+import express from 'express';
+import mainController from '../controllers/main';
+
+const mainRouter = express.Router();
+
 
 
 //// Frontend form
-router.get('/', mainController.root);
+mainRouter.get('/', mainController.root);
 
 // Get parameter from url that is shortened
 // if exist then response to the client with original url
 // response 200 if success and 404 for resource not found
-app.get('/:string', mainController.getUrl);
+// mainRouter.get('/:string', mainController.getUrl);
+
+module.exports = mainRouter;
