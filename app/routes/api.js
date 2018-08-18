1b3804202bf722c3d9bc9c2ba6b69806acadca5d
Pathompong Pechkongtong <boynoiz@gmail.com>
2018-08-18 11:54:29 +0700

    api route testing

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,9 +1,21 @@
-const express = require('express');
+import express from 'express';
+
 const apiRouter = express.Router();
+
+// Testing route
+apiRouter.get('/hello', function(request, response) {
+  response.send('Hello, My name is API');
+})
 
 //// API Backend
 // Get parameter from form action and then generate random string
 // store original url and random string in database and response shorten link to client
-apiRouter.post('/create/:string', function (request, response) {
-
+apiRouter.post('/create', (request, response) => {
+  const data = {
+    outputUrl: request.body.inputUrl,
+    message: "OK"
+  }
+  response.json(data);
 });
+
+module.exports = apiRouter;
