89f3533c1fc6ae44b893610c089c123d6f8df19b
Pathompong Pechkongtong <boynoiz@gmail.com>
2018-08-18 11:26:48 +0700

    Add express-vue middleware, import and use env setting

--- app/server.js
+++ app/server.js
@@ -1,15 +1,31 @@
-const express = require('express');
-const subdomain = require('express-subdomain');
+import path from 'path';
+import express from 'express';
+import expressVue from 'express-vue';
+import subdomain from 'express-subdomain';
+import mainRouter from './routes/main';
+import apiRouter from './routes/api';
+import config from './config';
+
+// Start the engine
 const app = express();
 
-const main = require('./routes/main');
-const api = require('./routes/api');
+// Use express-vue to render vue template and components
+const vueOptions = {
+  rootPath: path.join(__dirname, 'views')
+}
+
+const expressVueMiddleware = expressVue.init(vueOptions);
+app.use(expressVueMiddleware);
 
 // Setup mongoose connection
 
-app.use('/', main);
-app.use(subdomain('api', api));
+// Routing
+app.use('/', mainRouter);
+app.use(subdomain('api', apiRouter));
 
+// Listening
+app.listen(config.port, ()=> {
+  console.log('App start listening on port ' + config.port)
+});
 
-
-
+module.exports = app;
