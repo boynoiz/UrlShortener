731319e56fb7d810d15dbddecc334bae0c028e93
Pathompong Pechkongtong <boynoiz@gmail.com>
2018-08-19 09:52:26 +0700

    fix typo

--- app/models/config.js
+++ app/models/config.js
@@ -3,7 +3,7 @@ class Config {
     this.env = process.env.NODE_ENV || "development";
     this.url = process.env.APP_URL || "localhost";
     this.port = process.env.APP_PORT || 8080;
-    this.db_host = process.env.DB_HOST || "localhost";
+    this.db_host = process.env.APP_DB_HOST || "localhost";
   }
 }
 module.exports = Config;
