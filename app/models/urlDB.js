b7af246d84b06ef0d0769eb52f7cf03d8c4a9d99
Pathompong Pechkongtong <boynoiz@gmail.com>
2018-08-19 10:22:02 +0700

    Add createdAt

--- app/models/urlDB.js
+++ app/models/urlDB.js
@@ -1,9 +1,16 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+
 const Schema = mongoose.Schema;
 
 const UrlSchema = new Schema({
-  original_url: {type: String, required: true},
-  shorten_url: {type: String, required: true, max: 8},
+  originalUrl: {type: String, required: true},
+  shortenUrl: {type: String, required: true, max: 8},
+  createdAt: {type: Date, default: Date.now}
 });
+
+UrlSchema.methods = {
+// TODO
+// make 'insert' and 'find' method
+}
 
 module.exports = mongoose.model('UrlDB', UrlSchema);
