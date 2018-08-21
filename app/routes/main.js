9c247457128be063b51a635bfbbd8dcb6972b9f2
Pathompong Pechkongtong <boynoiz@gmail.com>
2018-08-21 18:07:38 +0700

    Open up!!

--- app/routes/main.js
+++ app/routes/main.js
@@ -11,6 +11,6 @@ mainRouter.get('/', mainController.root);
 // Get parameter from url that is shortened
 // if exist then response to the client with original url
 // response 200 if success and 404 for resource not found
-// mainRouter.get('/:string', mainController.getUrl);
+mainRouter.get('/:string', mainController.getUrl);
 
 module.exports = mainRouter;
