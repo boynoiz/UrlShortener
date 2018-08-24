3f04010f78c0734575360283af902c1332e17cf6
Pathompong Pechkongtong <boynoiz@gmail.com>
2018-08-24 22:28:31 +0700

    Tuning webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,87 @@
 const path = require('path');
-const { VueLoaderPlugin } = require('vue-loader');
+const {
+  VueLoaderPlugin
+} = require('vue-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-const env = process.env.NODE_ENV;
-const sourceMap = env === 'development';
-
-module.exports = [
-  /**
-   * Webpack config for Client-Side
-   */
-  {
-    entry: path.join(__dirname, 'views', 'app.js'),
-    mode: env,
-    output: {
-      path: __dirname + '/public/assets/js',
-      filename: 'app.js'
+module.exports = (env, argv) => ({
+  entry: path.join(__dirname, 'views', 'app.js'),
+  mode: argv.mode,
+  output: {
+    path: __dirname + '/public/assets/js',
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].[chunkhash].js'
+  },
+  optimization: {
+    splitChunks: {
+      // Must be specified for HtmlWebpackPlugin to work correctly.
+      // See: https://github.com/jantimon/html-webpack-plugin/issues/882
+      chunks: 'all',
     },
-    optimization: {
-      splitChunks: {
-        // Must be specified for HtmlWebpackPlugin to work correctly.
-        // See: https://github.com/jantimon/html-webpack-plugin/issues/882
-        chunks: 'all',
-      },
+    runtimeChunk: {
+      name: 'manifest'
     },
-    devtool: sourceMap ? 'cheap-module-eval-source-map' : undefined,
-    module: {
-      rules: [{
-          test: /\.vue$/,
-          loader: 'vue-loader',
-        },
-        {
-          test: /\.js$/,
-          loader: 'babel-loader',
-          include: [path.join(__dirname, 'views')],
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader']
+    minimizer: argv.mode === 'production' ? [
+      new OptimizeCSSAssetsPlugin(),
+      new UglifyJsPlugin({
+        parallel: true,
+        uglifyOptions: {
+          ie8: false,
+          ecma: 6,
+          parse: {},
+          mangle: {},
+          output: {
+              comments: false,
+              beautify: false,
+          },
+          compress: {
+              drop_console: true,
+              ecma : 6,
+          },
+          warnings: false
         }
-      ],
-    },
-    plugins: [
-      new Dotenv(),
-      new VueLoaderPlugin(),
-      new HtmlWebpackPlugin({
-        filename: path.join(__dirname, 'public', 'index.html'),
-        template: path.join(__dirname, 'views/template', 'index.html'),
-        inject: true,
       }),
+    ] : []
+  },
+  devtool: argv.mode === 'development' ? 'cheap-module-eval-source-map' : undefined,
+  module: {
+    rules: [{
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [path.join(__dirname, 'views')],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
     ],
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: 1000,
-      ignored: /node_modules/
-    },
-
-  }
-];
+  },
+  plugins: [
+    new Dotenv(),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      filename: path.join(__dirname, 'public', 'index.html'),
+      template: path.join(__dirname, 'views/template', 'index.html'),
+      inject: true,
+      minify: argv.mode === 'production' ? {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+        // More options:
+        // https://github.com/kangax/html-minifier#options-quick-reference
+      } : false,
+    }),
+  ],
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000,
+    ignored: /node_modules/
+  },
+});
