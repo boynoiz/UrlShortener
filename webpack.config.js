9164ee0a074455ee48a0225b48335cb39b4ec85e
Pathompong Pechkongtong <boynoiz@gmail.com>
2018-08-25 14:20:01 +0700

    Customizing for production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const glob = require('glob');
 const {
   VueLoaderPlugin
 } = require('vue-loader');
@@ -6,28 +7,37 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PurgecssPlugin = require('purgecss-webpack-plugin');
 
 module.exports = (env, argv) => ({
   entry: path.join(__dirname, 'views', 'app.js'),
   mode: argv.mode,
   output: {
-    path: __dirname + '/public/assets/js',
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].[chunkhash].js'
+    path: __dirname + '/public/assets',
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].bundle.js'
   },
   optimization: {
     splitChunks: {
       // Must be specified for HtmlWebpackPlugin to work correctly.
       // See: https://github.com/jantimon/html-webpack-plugin/issues/882
       chunks: 'all',
-    },
-    runtimeChunk: {
-      name: 'manifest'
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
     },
     minimizer: argv.mode === 'production' ? [
       new OptimizeCSSAssetsPlugin(),
       new UglifyJsPlugin({
+        cache: true,
         parallel: true,
+        sourceMap: false,
         uglifyOptions: {
           ie8: false,
           ecma: 6,
@@ -59,7 +69,11 @@ module.exports = (env, argv) => ({
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [
+          //This plugin should be used only on production builds without style-loader in the loaders chain
+          argv.mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader'
+        ]
       }
     ],
   },
@@ -77,6 +91,13 @@ module.exports = (env, argv) => ({
         // More options:
         // https://github.com/kangax/html-minifier#options-quick-reference
       } : false,
+    }),
+    new MiniCssExtractPlugin({
+      filename: argv.mode === 'development' ? "[name].css" : "[name].[hash].css",
+      chunkFilename: argv.mode === 'development' ? "[id].css" : "[id].[hash].css"
+    }),
+    new PurgecssPlugin({
+      paths: glob.sync(path.join(__dirname, 'views') + '/**/*',  { nodir: true }),
     }),
   ],
   watchOptions: {
