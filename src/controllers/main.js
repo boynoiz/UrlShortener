c5cdbf5c51d58add0425c7fef31a0495d1aa20bb
Pathompong Pechkongtong <boynoiz@gmail.com>
2018-08-27 16:22:34 +0700

    Redirect to 404 page when shorten string not found in database

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,28 +1,46 @@
 import path from 'path';
-import UrlModel from '../models/urlModel';
+import mongoose from "mongoose";
+import UrlSchema from '../models/UrlModel';
+
+const UrlModel = mongoose.model('Url', UrlSchema);
 
 const publicPath = path.join(process.cwd(), 'public');
+const templatePath = path.join(process.cwd(), 'views/template')
 
 
-//First page
-exports.root = (request, response) => {
-  response.sendFile(publicPath + '/index.html')
-};
+/**
+ * First page
+ *
+ * @param {Object} request
+ * @param {Object} response
+ * @returns {Object} response The response object
+ */
+export function root(request, response) {
+  response.sendFile(`${publicPath}/index.html`)
+}
 
-//Find string from db and redirect original url
-exports.getUrl = (request, response) => {
+/**
+ * Find string from db and redirect original url
+ *
+ * @param {Object} request
+ * @param {Object} response
+ *
+ * TODO:
+ * [x]Response to error page if no data exist
+ */
+export function getUrl(request, response) {
+
   const requestString = request.params.string;
+
   UrlModel.findOne({shortenUrl: requestString})
   .then((urlData) => {
     if (urlData) {
       response.status(301).redirect(urlData.originalUrl);
     } else {
-      //TODO
-      //Response to error page if no data exist
-      console.log("No data exist")
+      response.status(404).sendFile(`${templatePath}/404.html`)
     }
   })
   .catch((error) => {
     console.log("Errors: " + error);
   });
-};
+}
