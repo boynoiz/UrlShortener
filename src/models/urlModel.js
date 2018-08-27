1f072a0667b930beb72a32230783345c9a0c975d
Pathompong Pechkongtong <boynoiz@gmail.com>
2018-08-27 16:24:13 +0700

    Try to validate shorten string before save to database, but seems won't work?

--- src/models/urlModel.js
+++ src/models/urlModel.js
@@ -1,13 +1,30 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
+/**
+ *
+ */
 const Schema = mongoose.Schema;
 
 const UrlSchema = new Schema({
-  originalUrl: {type: String, required: true},
-  shortenUrl: {type: String, unique: true, required: true},
-  createdAt: {type: Date, default: Date.now}
+  originalUrl: { type: String, required: true },
+  shortenUrl: { type: String, unique: true, required: true },
+  createdAt: { type: Date, default: Date.now }
 });
 
-const UrlModel = mongoose.model('url', UrlSchema);
+UrlSchema.path("shortenUrl").validate(function(value, response) {
 
-module.exports = UrlModel;
+  const Url = mongoose.model("Url");
+
+  if (this.isNew || this.isModified("shortenUrl")) {
+    Url.count({ shortenUrl: value })
+    .exec()
+    .then(function(count) {
+      return !count;
+    })
+    .catch (function (error) {
+      throw error;
+    });
+  }
+}, 'Shorten word already exists');
+
+export default UrlSchema;
