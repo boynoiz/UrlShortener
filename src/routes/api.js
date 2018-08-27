c425faa681defddc3b8ae6ecd921343d5bde4504
Pathompong Pechkongtong <boynoiz@gmail.com>
2018-08-27 16:24:33 +0700

    Add words route for word-generator

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,16 +1,26 @@
 import { Router } from 'express';
-import APIController from '../controllers/api';
+import {create, words} from '../controllers/api';
 
 const apiRouter = Router();
 
-// Testing route
+/**
+ * Testing route
+ */
 apiRouter.get('/hello', function(request, response) {
   response.send('Hello, My name is API');
 });
 
-//// API Backend
-// Get parameter from form action and then generate random string
-// store original url and random string in database and response shorten link to client
-apiRouter.post('/create', APIController.create);
+/**
+ * API Back-End
+ * Get parameter from form action and then generate random string
+ * store original url and random string in database and response shorten link to client
+ */
+apiRouter.post('/create', create);
+
+/**
+ *
+ *
+ */
+apiRouter.get('/words', words);
 
 module.exports = apiRouter;
